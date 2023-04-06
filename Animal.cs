3057fe47f8dd9d6b44d501552e3a4277cf3f47ee
WCramRTC <91401050+WCramRTC@users.noreply.github.com>
2023-04-05 20:10:05 -0700

    Initial commit

---
+++ Animal.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_1
+{
+    internal class Animal
+    {
+
+        string _name;
+        string _breed;
+        int _legs;
+
+        public string Name { get => _name; set => _name = value; }
+        public string Breed { get => _breed; set => _breed = value; }
+        public int Legs { get => _legs; set => _legs = value; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Assignment_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal animal = new Animal()
+            {
+                Name = "Mochi",
+                Breed = "Tux Cat",
+                Legs = 4
+            };
+            Console.WriteLine($"{animal.Name} {animal.Breed} {animal.Legs}");
+        }
+    }
+}
